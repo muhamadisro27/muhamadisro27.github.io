d079e387ae396c2cb5e54f786332354b74239e7e
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-10-17 20:56:03 +0700

    feat: set static title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Muhamad Isro</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
