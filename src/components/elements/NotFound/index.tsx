bc5f8f2dad3b8eed9fcd962bc9208e32e33194aa
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-10-20 19:54:50 +0700

    fix: escape string

--- src/components/elements/NotFound/index.tsx
+++ src/components/elements/NotFound/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.scss";
 const NotFoundError = ({ keyword }: { keyword: string }) => {
   return (
     <div className={styles.notFound}>
-      "No results found for the keyword: '{keyword}'"
+      {`No results found for the keyword: '${keyword}'`}
     </div>
   );
 };
