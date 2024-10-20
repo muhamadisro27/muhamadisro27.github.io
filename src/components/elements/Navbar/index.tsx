be81a008a1da30eb7bdd3dd3374e24c390d9b5cc
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-10-20 20:47:49 +0700

    feat: add responsive page home

--- src/components/elements/Navbar/index.tsx
+++ src/components/elements/Navbar/index.tsx
@@ -1,20 +1,39 @@
 import Link from "next/link";
 import styles from "./index.module.scss";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const { pathname } = useRouter();
+
+  const menus = [
+    {
+      url: "/",
+      name: "Home",
+    },
+    {
+      url: "/projects",
+      name: "Projects",
+    },
+    {
+      url: "/posts",
+      name: "Post",
+    },
+  ];
+
   return (
     <nav className={styles.nav}>
       <h1>Roozyroo</h1>
       <ul>
-        <li>
-          <Link href="/">Home</Link>
+        {menus.map((menu) => (
+          <li key={menu.url}>
+            <Link
+              href={menu.url}
+              className={`${menu.url == pathname ? "active" : ""}`}
+            >
+              {menu.name}
+            </Link>
           </li>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/posts">Post</Link>
-        </li>
+        ))}
       </ul>
     </nav>
   );
