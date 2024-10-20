616c11a6a36126d0910f25a07ad3abebaa9f58c6
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-10-20 21:23:37 +0700

    feat: add feature responsive navbar

--- src/components/elements/Navbar/index.tsx
+++ src/components/elements/Navbar/index.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import styles from "./index.module.scss";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import MenuIcon from "../MenuIcon";
+import CloseIcon from "../CloseIcon";
 
 const Navbar = () => {
   const { pathname } = useRouter();
@@ -20,9 +23,27 @@ const Navbar = () => {
     },
   ];
 
+  const [activeMenu, setActiveMenu] = useState(false);
+
+  const handleClickMenu = () => {
+    setActiveMenu(!activeMenu);
+  };
+
   return (
     <nav className={styles.nav}>
       <h1>Roozyroo</h1>
+      <div className={styles.hamburger} onClick={handleClickMenu}>
+        {activeMenu ? <CloseIcon /> : <MenuIcon />}
+      </div>
+      {/* <div
+        className={`${
+          activeMenu ? styles.listMenuMobileActive : styles.listMenuMobile
+        }`}
+      >
+        <ul>
+          <li>asd</li>
+        </ul>
+      </div> */}
       <ul>
         {menus.map((menu) => (
           <li key={menu.url}>
