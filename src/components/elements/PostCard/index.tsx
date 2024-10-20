5fee4b9e6cd1615251a04df13901282ef94e71d4
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-10-20 16:29:34 +0700

    fix: wrapper style post card

--- src/components/elements/PostCard/index.tsx
+++ src/components/elements/PostCard/index.tsx
@@ -13,7 +13,7 @@ const PostCard = (props: PostURL) => {
         <div className={styles.imageWrapper}>
           <Image src={useImage(thumbnail)} alt="" fill objectFit="cover" quality={80} />
         </div>
-        <h3>{title}</h3>
+        <h3 className={styles.title} title={title}>{title}</h3>
         <p className={styles.summary}>{summary}</p>
       </Link>
     </section>
