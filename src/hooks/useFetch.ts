be2f4a46a3bfc6f55b333bfdad65869ca45a5f99
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-10-21 10:29:59 +0700

    update: remove comment where clauses

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,18 +3,9 @@ import { PostgrestError } from "@supabase/supabase-js";
 
 export const fetchAll = async (
   table: string,
-  // whereClause = {} as WhereClause,
   limit: number = 0
 ) => {
   let query = supabase.from(table).select("*");
-
-  // const whereClauses = Object.keys(whereClause);
-
-  // if (whereClauses.length > 0) {
-  //   for (const key in whereClauses) {
-  //     // TODO: Refactor where clauses
-  //   }
-  // }
 
   if (limit > 0) {
     query = query.limit(limit);
